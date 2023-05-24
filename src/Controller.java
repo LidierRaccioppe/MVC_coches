196bd8706dcd02549e420a45d321faa526d05be5
Lidier López <llopezraccioppe@danielcastelao.org>
2023-05-24 14:32:15 +0200

    activo la vista en el controlador

--- src/Controller.java
+++ src/Controller.java
@@ -10,29 +10,9 @@ public class Controller {
 
         public static void main(String[] args) {
 
-                // el patron Observer en java nos exige instanciar la clase observable
-                miModelo = new Model();
 
-                // instanciamos al observador
-                ObserverVelocidad observoVelocidad = new ObserverVelocidad();
-                miModelo.addObserver(observoVelocidad);
-
-                // instanciamos un segundo observador
-                ObserverOtro otroObservador = new ObserverOtro();
-                miModelo.addObserver(otroObservador);
-
-                // Crear tres coches
-
-                miModelo.crearCoche("LaFerrari", "SBC 1234");
-                miModelo.crearCoche("Alpine", "HYU 4567");
-                miModelo.crearCoche("Aston Martin", "FGH 3333");
-
-                Coche ferrari = Model.getCoche("SBC 1234");
-                // modifica la velocidad
-                miModelo.subirVelocidad("SBC 1234", 30);
-
-                // otro cambio de velocidad
-                miModelo.bajarVelocidad("HYU 4567", 100);
+                miVista = new View();
+                System.out.println("fin del mundo");
 
         }
 
