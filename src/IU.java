c8cf09aa5ae6744d3d44dae340a1a658f126ba60
Lidier López <llopezraccioppe@danielcastelao.org>
2023-05-24 13:59:21 +0200

    agrego evento buscar

--- src/IU.java
+++ src/IU.java
@@ -15,11 +15,14 @@ public class IU implements ActionListener {
         private JPanel panel;
         private JLabel etiquetaMatricula;
         private JLabel etiquetaModelo;
+        private JLabel etiquetaBuscar;
         private JTextField textoMatricula;
         private JTextField textoModelo;
+        private JTextField textoBuscar;
         private JButton botonCrearCoche;
         private JButton botonSubirVelocidad;
         private JButton botonBajarVelocidad;
+        private JButton botonBuscar;
 
         /**
          * Function that begin the components for the UserInterface
@@ -30,16 +33,20 @@ public class IU implements ActionListener {
                 panel = new JPanel();
                 etiquetaMatricula = new JLabel("Ingrese la Matricula, porfavor");
                 etiquetaModelo = new JLabel("Ingrese el Modelo, porfavor");
+                etiquetaBuscar = new JLabel("Ingrese el Matricula a buscar, porfavor");
                 textoMatricula = new JTextField();
                 textoModelo = new JTextField();
+                textoBuscar = new JTextField();
                 botonCrearCoche = new JButton("Crear Coche");
                 botonSubirVelocidad = new JButton("Subir Velocidad");
                 botonBajarVelocidad = new JButton("Bajar Velocidad");
+                botonBuscar = new JButton("Buscar Coche");
 
                 //Gestón de Eventos
                 botonCrearCoche.addActionListener(this); // en la misma clase
                 botonSubirVelocidad.addActionListener(this); // en la misma clase
                 botonBajarVelocidad.addActionListener(this); // en la misma clase
+                botonBuscar.addActionListener(this); // en la misma clase
         }
 
 
@@ -53,9 +60,12 @@ public class IU implements ActionListener {
                 panel.add(textoMatricula);
                 panel.add(etiquetaModelo);
                 panel.add(textoModelo);
+                panel.add(etiquetaBuscar);
+                panel.add(textoBuscar);
                 panel.add(botonCrearCoche);
                 panel.add(botonSubirVelocidad);
                 panel.add(botonBajarVelocidad);
+                panel.add(botonBuscar);
                 marco.add(panel);
                 computoVentana();
         }
@@ -71,7 +81,7 @@ public class IU implements ActionListener {
         }
 
         /**
-         * Event to create a Coche
+         * Event to create, decreace/increase the speed or search a Coche
          * @param e the event to be processed
          */
         @Override
@@ -93,6 +103,11 @@ public class IU implements ActionListener {
                         //respetando a MVC
                         con.bajarVelocidad(textoMatricula.getText(), ve);
                         System.out.println("Velocidad bajada");
+                }
+                else if (botonPorPerformirizar == botonBuscar){
+                        //respetando a MVC
+                        con.buscarYMostrar(textoMatricula.getText());
+                        System.out.println("Coche Mostrado");
                 }System.exit(0);
         }
 }
